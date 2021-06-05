ec2b2e10109886fadf5e145403e53db34dbc246b
Abastro <abab9579@naver.com>
2021-06-05 22:43:33 +0900

    Stack?

--- flow.py
+++
@@ -1,9 +0,0 @@
-class BasicBlock:
-    def __init__(self, lines):
-        self.lines = lines
-
-class Line:
-    def __init__(self, lineNum, ast):
-        self.lineNum = lineNum
-        self.ast = ast
-
---
+++ stack.py
@@ -0,0 +1,10 @@
+class CallStack:
+    def __init__(self):
+        self.called = []
+        pass
+
+class ValueStack:
+    def __init__(self):
+        self.values = []
+        pass
+
