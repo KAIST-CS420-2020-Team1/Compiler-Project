87e5fc19ca569f3fd643a13787d02b0933c0d5de
Abastro <abab9579@naver.com>
2021-06-07 22:09:38 +0900

    Began working on parse

--- parse.py
+++ parse.py
@@ -1,363 +1,20 @@
 import ply.yacc as yacc
 
-class NullNode(Node):
-    """A null node is like a null terminator for AST's."""
-
-    def __init__(self):
-        self.type = 'void'
-
-    def is_null(self):
-        return 1
-
-class ArrayExpression(Node):
-    """This is an expression with array notation, like "a[5+b]"."""
-
-    def __init__(self, expr, index):
-        self.expr = expr
-        self.index = index
-
-class StringLiteral(Node):
-    """A string literal, e.g. the string "Hello World" in
-    printf("Hello World")."""
-    
-    def __init__(self, str):
-        self._str = str
-        self.type = PointerType(BaseType('char'))
-
-    def append_str(self, str):
-        self._str += str
-    
-    def get_str(self):
-        return self._str
-    
-    def get_sanitized_str(self):
-        """Returns a 'sanitized' version of the string, converting
-        all carriage returns to '\n' symbols, etc."""
-
-        return self._str.replace('\n', '\\n')
-
-class Id(Node):
-    """An identifier, which can correspond to the name of
-    a function, variable, etc..."""
-
-    def __init__(self, name, lineno):
-        self.name = name
-        self.lineno = lineno
-
-class Const(Node):
-    """A numeric constant (i.e., an integral literal), such as
-    the number 5."""
-    
-    def __init__(self, value, type):
-        self.value = value
-        self.type = type
-
-    def calculate(self):
-        return self.value
-
-    def is_const(self):
-        return 1
-
-def _get_calculated(node):
-    """Attempts to calculate the numeric value of the expression,
-    returning a Const node if it was able to convert the expression.
-    If the expression isn't a constant expression like "5+3", then
-    this function just returns the node unmodified."""
-
-    result = node.calculate()
-    if result != None:        
-        result = int(result)
-        return Const(result, BaseType('int'))
-    else:
-        return node
-
-class Unaryop(Node):
-    """Any generic unary operator.  This is an abstract base class."""
-    
-    def __init__(self, node):
-        self.expr = node
-
-class Negative(Unaryop):
-    """A negative unary operator, e.g. '-5'."""
-    
-    def calculate(self):
-        val = self.expr.calculate()
-        if val != None:
-            return -val
-        return None
-
-class Pointer(Unaryop):
-    """A pointer dereference, e.g. '*a'."""
-
-    pass
-
-class AddrOf(Unaryop):
-    """An address-of operator, e.g. '&a'."""
-    
-    pass
-
-class Binop(Node):
-    """Any binary operator, such as that for arithmetic operations
-    (+/-/*), assignment operations (=/+=/-=), and so forth."""
-
-    # List of assignment operators.
-    ASSIGN_OPS = ['=', '+=', '-=']
-    
-    def __init__(self, left, right, op):
-        self.left = left
-        self.right = right
-        self.op = op
-
-    def calculate(self):
-        left = self.left.calculate()
-        right = self.right.calculate()
-        if left != None and right != None:
-            return int(eval("%d %s %d" % (left, self.op, right)))
-        else:
-            return None
-
-class IfStatement(Node):
-    """An if/then/else statement."""
-    
-    def __init__(self, expr, then_stmt, else_stmt):
-        self.expr = expr
-        self.then_stmt = then_stmt
-        self.else_stmt = else_stmt
-
-class BreakStatement(Node):
-    """A break statement (used while in a loop structure to bust out
-    of it)."""
-
-    pass
-
-class ContinueStatement(Node):
-    """A continue statement (used while in a loop structure to bust
-    back to the beginning of it)."""
-    
-    pass
-
-class ReturnStatement(Node):
-    """A return statement, used to exit a function and optionally
-    return a value."""
-    
-    def __init__(self, expr):
-        self.expr = expr
-
-class ForLoop(Node):
-    """A for loop."""
-    
-    def __init__(self, begin_stmt, expr, end_stmt, stmt):
-        self.expr = expr
-        self.stmt = stmt
-        self.begin_stmt = begin_stmt
-        self.end_stmt = end_stmt
-
-class WhileLoop(Node):
-    """A while loop."""
-    
-    def __init__(self, expr, stmt):
-        self.expr = expr
-        self.stmt = stmt
-
-class NodeList(Node):
-    """A list of nodes.  This is an abstract base class."""
-    
-    def __init__(self, node=None):
-        self.nodes = []
-        if node != None:
-            self.nodes.append(node)
-
-    def add(self, node):
-        self.nodes.append(node)
-
-class ArgumentList(NodeList):
-    """A list of arguments for a function expression.  e.g., the list
-    '5,2,3' in 'a = my_func(5,2,3)'."""
-    
-    pass
-
-class ParamList(NodeList):
-    """A list of parameters for a function prototype, e.g. the list
-    'int a, char b, char c' in 'int my_func(int a, char b, char c)'."""
-
-    def __init__(self, node=None):
-        NodeList.__init__(self, node)
-        self.has_ellipsis = 0
-
-class StatementList(NodeList):
-    """Any list of statements.  For instance, this can be the list of
-    statements in a function body."""
-
-    pass
-
-class TranslationUnit(NodeList):
-    """A list of nodes representing the program itself."""
-
-    pass
-
-class DeclarationList(NodeList):
-    """A list of variable declarations, such as the ones put
-    at the beginning of a compound statement (e.g., the beginning
-    of a function body)."""
-    
-    pass
-
-class FunctionExpression(Node):
-    """An execution of a function, e.g. 'my_func(a,b,c)'."""
-    
-    def __init__(self, function, arglist):
-        self.function = function
-        self.arglist = arglist
-
-class CompoundStatement(Node):
-    """A compound statement, e.g. '{ int i; i += 1; }'."""
-    
-    def __init__(self, declaration_list, statement_list):
-        self.declaration_list = declaration_list
-        self.statement_list = statement_list
-
-class FunctionDefn(Node):
-    """A node representing a function definition (its declaration
-    and body)."""
-    
-    def __init__(self, declaration, body):
-        self.type = declaration.type
-        self.name = declaration.name
-        self.extern = declaration.extern
-        self.static = declaration.static
-        self.body = body
-
-class Declaration(Node):
-    """A node representing a declaration of a function or
-    variable."""
-
-    def __init__(self, name, type=None):
-        if type == None:
-            type = NullNode()
-        self.extern = 0
-        self.static = 0
-        self.type = type
-        self.name = name
-        self.is_used = 0
-
-    def set_base_type(self, type):
-        if self.type.is_null():
-            self.type = type
-        else:
-            self.type.set_base_type(type)
-
-    def add_type(self, type):
-        type.set_base_type(self.type)
-        self.type = type
-
-class Type(Node):
-    """A node representing the type of another node.  For instance,
-    the Binop node representing '5 + a', where a is an int, will have
-    a Type node associated with it that represents the fact that
-    the result of the Binop is an int.
-    Types can also be nested, so that for instance you can have
-    a type like 'pointer(pointer(int))' which represents a
-    double-pointer to an int.
-    This is an abstract base class."""
-    
-    def __init__(self, child=None):
-        if child == None:
-            child = NullNode()
-        self.child = child
-
-    def set_base_type(self, type):
-        """Set the base (innermost) type of a type.  For instance,
-        calling this with a pointer(int) type on a pointer() type
-        will give you a pointer(pointer(int))."""
-        
-        if self.child.is_null():
-            self.child = type
-        else:
-            self.child.set_base_type(type)
-
-    def get_string(self):
-        """Return a string corresponding to the type, e.g.
-        'pointer(pointer(int))'."""
-        
-        raise NotImplementedError()
-
-    def get_outer_string(self):
-        """Return only the outermost type of a type.  e.g.,
-        calling this on a pointer(pointer(int)) type will
-        return 'pointer'."""
-        
-        raise NotImplementedError()
-
-    def is_function(self):
-        """Returns whether or not this type represents a
-        function."""
-        
-        return 0
-
-class BaseType(Type):
-    """A base type representing ints, chars, etc..."""
-    
-    def __init__(self, type_str, child=None):
-        Type.__init__(self, child)
-        self.type_str = type_str
-
-    def get_string(self):
-        return self.type_str
-
-    def get_outer_string(self):
-        return self.type_str
-
-class FunctionType(Type):
-    """A type representing a function (for function prototypes and
-    function calls)."""
-    
-    def __init__(self, params=None, child=None):
-        Type.__init__(self, child)
-        if (params == None):
-            params = NullNode()
-        self.params = params
-
-    def get_string(self):
-        param_str = ""
-        for param in self.params.nodes:
-            param_str += "," + param.type.get_string()
-        return "function(%s)->%s" % (param_str[1:], self.child.get_string())
-
-    def get_outer_string(self):
-        return 'function'
-
-    def is_function(self):
-        return 1
-
-    def get_return_type(self):
-        """Returns the return type of the function.  Internally,
-        this is stored as the nested type within the function."""
-        
-        return self.child
-
-    def get_params(self):
-        """Returns the list of parameters for the function."""
-        
-        return self.params
-
-class PointerType(Type):
-    """A type representing a pointer to another (nested) type."""
-    
-    def get_string(self):
-        return "pointer(%s)" % self.child.get_string()
-
-    def get_outer_string(self):
-        return 'pointer'
-
-
 precedence = (
     ('right', 'ELSE'),
 )
 
+class TranslationUnit:
+    def __init__(self, decl):
+        self.decls = [decl]
+    def add(self, decl):
+        self.decls = self.decls + [decl]
+        return self
+    def decls(self):
+        return self.decls
+
 class ParseError(Exception):
     "Exception raised whenever a parsing error occurs."
-
-    pass
 
 def p_translation_unit_01(t):
     '''translation_unit : external_declaration'''
@@ -365,58 +22,39 @@ def p_translation_unit_01(t):
 
 def p_translation_unit_02(t):
     '''translation_unit : translation_unit external_declaration'''
-    t[1].add(t[2])
-    t[0] = t[1]
+    t[0] = t[1].add(t[2])
 
 def p_external_declaration(t):
     '''external_declaration : function_definition
                             | declaration'''
     t[0] = t[1]
 
-def p_function_definition_01(t):
+
+class FunctionDefn():
+    def __init__(self, r_type, declarator, body):
+        self.r_type = r_type
+        self.declarator = declarator
+        self.body = body
+        pass
+
+def p_function_definition(t):
     '''function_definition : type_specifier declarator compound_statement'''
     t[2].set_base_type(t[1])
-    t[0] = FunctionDefn(t[2], t[3])
+    t[0] = FunctionDefn(t[2], t[3]) # Need to change this
 
-def p_function_definition_02(t):
-    '''function_definition : STATIC type_specifier declarator compound_statement'''
-    t[3].static = 1
-    t[3].set_base_type(t[2])
-    t[0] = FunctionDefn(t[3], t[4])
-    
+def Declaration():
+    def __init__(self, base_type, declarator):
+        self.base_type = base_type
+        self.declarator = declarator
+        pass
+
 def p_declaration_01(t):
     '''declaration : type_specifier declarator SEMICOLON'''
-    if isinstance(t[2].type, FunctionType):
-        t[2].extern = 1
-    t[2].set_base_type(t[1])
-    t[0] = t[2]
+    t[0] = Declaration(t[1], t[2])
 
-def p_declaration_02(t):
-    '''declaration : EXTERN type_specifier declarator SEMICOLON'''
-    t[3].extern = 1
-    t[3].set_base_type(t[2])
-    t[0] = t[3]
-
-def p_declaration_list_opt_01(t):
-    '''declaration_list_opt : empty'''
-    t[0] = NullNode()
-
-def p_declaration_list_opt_02(t):
-    '''declaration_list_opt : declaration_list'''
-    t[0] = t[1]
-
-def p_declaration_list_02(t):
-    '''declaration_list : declaration'''
-    t[0] = DeclarationList(t[1])
-
-def p_declaration_list_03(t):
-    '''declaration_list : declaration_list declaration'''
-    t[1].add(t[2])
-    t[0] = t[1]
-    
 def p_type_specifier(t):
     '''type_specifier : INT
-                      | CHAR'''
+                      | FLOAT'''
     t[0] = BaseType(t[1])
 
 def p_declarator_01(t):
