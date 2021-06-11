a25f23e400f88c0b95ea54db5f8c70f9ecf08eb2
KAIST-JongchanPark <whdals1060@kaist.ac.kr>
2021-06-11 23:27:58 +0900

    main update

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ import structure
 import stack
 import analysis
 
+
 def val_str(value):
     if value == None:
         return 'N/A'
     else:
         return str(value)
+
+
 def history_str(var_name, history):
     if history == []:
         return "{} not yet initialized".format(var_name)
@@ -21,52 +24,61 @@ def history_str(var_name, history):
             "{} = {} at line {}".format(var_name, val_str(val), line)
             for (val, line) in history])
 
+
 class MainContext:
     def __init__(self, ast):
         # TODO CFG?
+        self.CFG = CFG.generate_graph(ast)
 
-        # Structures
         self.global_table = analysis.get_symbol_table(ast.decls)
-        self.func_tables = structure.Function_Table()
-        for fndecl in filter(analysis.is_instance(parse.FunctionDefn), ast.decls):
-            blocks = analysis.desugar_body(fndecl.body)
-            print("{}:\n{}".format(fndecl.declarator, blocks))
-            entry = analysis.get_function_entry(fndecl)
-            self.func_tables.insert(entry[0], entry[1], entry[2], entry[3], entry[4], None, stack.ValueTable())
-
-        # Stacks
-        self.val_stack = stack.ValueStack()
-        self.call_stack = stack.CallStack()
+        self.func_tables = CFG.function_table
+        self.val_stack = CFG.value_stack
+        self.call_stack = CFG.call_stack
 
     def begin(self):
         self.cur_func_table = self.func_tables.table["main"]
         pass
 
-    def next(self, num = 1):
+    def cmd_next(self, num=1):
         # TODO Runs #num statements
-        0
+        cfg_node = self.CFG[0].next_line()
 
-    def print(self, var_name):
-        vtable = self.cur_func_table.ref.ref
+        while (cfg_node != None):
+            cfg_node = cfg_node.next_line()
+
+        print("End of program")
+        return
+
+    def cmd_print(self, var_name):
+        vtable = self.cur_func_table.ref_value
+
         if vtable.has_value(var_name):
             print(val_str(vtable.get_value(var_name)))
             return
-        vtable = self.global_table.ref
+
+        '''vtable = self.global_table.ref.ref_value
+
         if vtable.has_value(var_name):
             print(val_str(vtable.get_value(var_name)))
-            return
-        print("{} not found".format(var_name))
+            return'''
 
-    def trace(self, var_name):
-        vtable = self.cur_func_table.ref.ref
+        print("N\A")
+        return
+
+    def cmd_trace(self, var_name):
+        vtable = self.cur_func_table.ref_value
+
         if vtable.has_value(var_name):
             print(history_str(var_name, vtable.get_history(var_name)))
             return
-        vtable = self.global_table.ref
+
+        '''vtable = self.global_table.ref
         if vtable.has_value(var_name):
             print(history_str(var_name, vtable.get_history(var_name)))
-            return
-        print("{} not found".format(var_name))
+            return'''
+
+        print("Invisible variable")
+        return
 
 
 if __name__ == '__main__':
@@ -75,6 +87,7 @@ if __name__ == '__main__':
     input_file = open(sys.argv[1])
     parsed = parser.parse(input_file.read(), tracking=True)
     input_file.close()
+    print(parsed)
     ctxt = MainContext(parsed)
 
     ctxt.begin()
@@ -87,11 +100,14 @@ if __name__ == '__main__':
         args = line.split(' ')
         if args[0] == 'next':
             if len(args) == 1:
-                ctxt.next()
+                ctxt.cmd_next()
             elif args[1].isnumeric():
-                ctxt.next(int(args[1])) # Placeholder
-        if args[0] == 'print' and len(args) == 2:
-            ctxt.print(args[1])
-        if args[0] == 'trace' and len(args) == 2:
-            ctxt.trace(args[1])
-
+                ctxt.cmd_next(int(args[1]))  # Placeholder
+            else:
+                print("Incorrect command usage: try 'next[lines]")
+        elif args[0] == 'print' and len(args) == 2:
+            ctxt.cmd_print(args[1])
+        elif args[0] == 'trace' and len(args) == 2:
+            ctxt.cmd_trace(args[1])
+        else:
+            print("Invalid typing of the variable name")
